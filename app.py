79a2d6dfeb2800de5941597df4dec4bd3b44b560
meetvorami <meet.vora@mindinventory.com>
2024-08-22 16:41:06 +0530

    adding middleware

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.routes.route import router
+from CustomMiddleware.middleware import CustomMiddleWare
 
 app = FastAPI()
 
+app.add_middleware(CustomMiddleWare)
 
 app.include_router(router=router)
